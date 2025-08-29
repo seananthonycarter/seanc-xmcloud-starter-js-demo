64cdd8ab8ca1d3e5834050f0d884aedbcb6340ed
Sean Carter <sean.carter@sitecore.com>
2025-08-29 08:51:12 +0100

    Dial down linting

--- examples/kit-nextjs-product-listing/next.config.js
+++ examples/kit-nextjs-product-listing/next.config.js
@@ -117,6 +117,11 @@ const nextConfig = {
 
     return config;
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 
   // Add sass settings for SXA themes and styles
 sassOptions: {
